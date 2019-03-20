36c1be87cedd26f98fbacccf21b47339fd8a67b7
AlexGinestra <alex.ginestra4@etu.univ-lorraine.fr>
2019-03-20 01:13:57 +0100

    debut A B, B A

--- wicopaco2csv/src/filter/RollbackFilter.java
+++ wicopaco2csv/src/filter/RollbackFilter.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import parser.ParserXML;
 
 public class RollbackFilter implements RejectionFilter{
 	
@@ -40,10 +43,75 @@ public class RollbackFilter implements RejectionFilter{
 			listBefAft = listTemp;
 		}
 	}
-
+	
+	/*
+	 * return the word number in the tag : <m>
+	 */
+	private int getWordNumber(Node mTag){
+		return Integer.parseInt(ParserXML.getAttributeContent(mTag.getAttributes(), "num_words"));
+	}
+	
+	
+	/*
+	 * return true if the word str is already in the list
+	 */
+	public boolean alreadySeen(String str, int wordNumber) {
+		if(wordNumber > listBefAft.length) {
+			return false;
+		}
+		
+		for(String[] strTab : listBefAft[wordNumber-1]) {
+			if(strTab[0].equals(str) || strTab[1].equals(str)) {
+				return true;
+			}
+		}
+		
+		
+		return false;
+	}
+	
+	
+	
+	
 	@Override
 	public boolean hasToBeRemoved(Node n) {
+
+		
 		// TODO Auto-generated method stub
+		NodeList nList = n.getChildNodes();
+		String before;
+		
+		/* Parcours les enfants de modif */
+		for(int j = 0 ; j < nList.getLength() ; j++) {
+			Node nTempBefAft = nList.item(j);
+			//balise before
+			if(nTempBefAft.getNodeName().equals("before")) {
+				NodeList lTemp = nTempBefAft.getChildNodes();
+				for(int i = 0 ; i < lTemp.getLength()-1 ; i++) {
+					if(lTemp.item(i).getNodeName().equals("m")) {
+						Node mTag = lTemp.item(i);
+						if(alreadySeen(mTag.getTextContent() ,getWordNumber(mTag)) ) {
+							return false;
+						}
+						else {
+							before = mTag.getTextContent();
+						}
+					}
+				}
+			}
+			//balise after
+			else if(nTempBefAft.getNodeName().equals("after")) {
+				NodeList lTemp = nTempBefAft.getChildNodes();
+				for(int i = 0 ; i < lTemp.getLength()-1 ; i++) {
+					if(lTemp.item(i).getNodeName().equals("m")) {
+						Node mTag = lTemp.item(i);
+						//TODO
+					}
+				}
+			}
+		}
+		
+		
 		return false;
 	}
 
